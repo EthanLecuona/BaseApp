8da6b816cdf892101daeb917bd023ab98bf020cd
EthanLecuona <ethanlecuona@gmail.com>
2024-02-04 20:32:33 +0200

    First Initial Commit
    Forgot to save some files before creating the repository.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -11,7 +11,6 @@ export default async function page () {
   }
   
   return (
- 
     <>
     <div className="container relative bg-muted hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
