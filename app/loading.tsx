0134c13a50d3dbf59d91acacd1861c83f898c524
EthanLecuona <ethanlecuona@gmail.com>
2024-02-24 17:20:29 +0200

    Update 1.2.1
    Just added a loader so that it looks less weird when you login and the DefaultNav disappears.

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Loging you in!</h1>
+        </div>
+    );
+}
