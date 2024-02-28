c036f67af8564cb5cc9b564e38c7aa2bfb387485
EthanLecuona <ethanlecuona@gmail.com>
2024-02-28 09:50:37 +0200

    This. Fucking. Sharp. Module.
    God...

--- next.config.mjs
+++ next.config.mjs
@@ -18,16 +18,16 @@ const nextConfig = {
                 pathname: '/**',
             },
         ]
-    },
-    webpack: (cfg, options = {}) => {
-        cfg.externals.push('sharp')
-        const { webpack } = options
-        const regex = /^sharp$/
-        cfg.plugins.push(new webpack.IgnorePlugin({
-          resourceRegExp: regex,
-        }))
-        return cfg;
     }
+    // webpack: (cfg, options = {}) => {
+    //     cfg.externals.push('sharp')
+    //     const { webpack } = options
+    //     const regex = /^sharp$/
+    //     cfg.plugins.push(new webpack.IgnorePlugin({
+    //       resourceRegExp: regex,
+    //     }))
+    //     return cfg;
+    // }
 };
 export default nextConfig;
 
